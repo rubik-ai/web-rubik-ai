9600e922361b728ea6135ee5f9c1bcd5a8f8a80f
ashish47b <ashish.sahu@47billion.com>
2019-01-08 19:14:24 +0530

    updated v5

--- beta/assets/js/typing-animation.js
+++ beta/assets/js/typing-animation.js
@@ -52,6 +52,5 @@ window.onload = function() {
     // INJECT CSS
     var css = document.createElement("style");
     css.type = "text/css";
-    css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
     document.body.appendChild(css);
 };
